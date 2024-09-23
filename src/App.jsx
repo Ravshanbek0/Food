90990e8df4689a9d09e5ffccc84b9cd9370af264
Ravshanbek0 <rnabijonov66@gmail.com>
2024-09-23 20:36:57 +0500

    1

---
+++ src/App.jsx
@@ -0,0 +1,248 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <header className="header">
+          <nav className="nav-bar">
+            <div className="nav-blogLeft">
+              <img src="././public/imgs/logo.png" alt="" className="logo-img" />
+              <ul className="nav-ul">
+                <li><a className="active" href="">Home</a></li>
+                <li><a href="">About us</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Features</a></li>
+                <li><a href="">Contact us</a></li>
+              </ul>
+            </div>
+            <div className="nav-blogRight">
+              <button className="booking">Booking now</button>
+            </div>
+
+          </nav>
+          <div className="header-main">
+            <div className="headerMain-left">
+              <h1>Making time a good time by making food the good food.</h1>
+              <p>There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also specialized equipment,</p>
+              <div className="hmainLeft-box">
+                <button className="order-now">Order Now</button>
+                <button className="food-details">Food Details</button>
+              </div>
+            </div>
+            <div className="headerMain-right">
+              <img src="././public/imgs/header img.png" alt="" />
+            </div>
+          </div>
+        </header>
+
+      </div>
+      <div className="section-strange">
+        <div className="strange-box">
+          <img src="../public/imgs/strange.png" alt="" />
+        </div>
+        <main className="main">
+          <div className="features">
+            <p className="featP">Features</p>
+            <h1 className="featText">Food with a New Passion</h1>
+            <div className="featBoxes">
+              <div className="feat-blog">
+                <div className="featuresAll">
+                  <img src="././public/imgs/uy.png" alt="" />
+                </div>
+                <h1>Quality Food</h1>
+                <p>It can be a very secure path to earn good money and make you very successful creative entrepreneur.</p>
+              </div>
+              <div className="feat-blog">
+                <div className="featuresAll">
+                  <img src="././public/imgs/food-delivery.png" alt="" />
+                </div>
+                <h1>Food Delivery</h1>
+                <p>It can be a very secure path to earn good money and make you very successful creative entrepreneur.</p>
+              </div>
+              <div className="feat-blog">
+                <div className="featuresAll">
+                  <img src="././public/imgs/smile.png" alt="" />
+                </div>
+                <h1>Super Taste</h1>
+                <p>It can be a very secure path to earn good money and make you very successful creative entrepreneur.</p>
+              </div>
+            </div>
+          </div>
+          <div className="aboutUs">
+            <div className="about-left">
+              <img src="././public/imgs/girl.png" alt="" />
+            </div>
+            <div className="about-right">
+              <h1 className="aboutTitle">About us</h1>
+              <h1 className="aboutText">Food Stalls with Persons but  to  Product marketing plane
+                catlogues etc to. </h1>
+              <p>There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also
+                equipment  make your marketing plane Effective.</p>
+              <button className="about-btn">Read More</button>
+            </div>
+          </div>
+        </main>
+      </div>
+      <div className="container">
+        <div className="menu">
+          <h2 className="menu-title">Menu</h2>
+          <h1 className="menu-text">Food Full of treaty Love</h1>
+          <p className="helper-title">There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, </p>
+          <div className="menu-blog">
+            <div className="menu-boxes">
+              <img src="././public/imgs/meals/chikken.png" alt="" />
+              <div className="menu-about">
+                <p>Vegie Muffen</p>
+                <p>16$</p>
+              </div>
+              <p className='menu-box-title'>There are many things are needed to start the Fast Food Business.</p>
+              <div className="menu-rating">
+                <button className='menu-add-btn'>
+                  +
+                </button>
+                
+              </div>
+            </div>
+            <div className="menu-boxes">
+              <img src="././public/imgs/meals/meat.png" alt="" />
+              <div className="menu-about">
+                <p>Salads</p>
+                <p>12$</p>
+              </div>
+              <p className='menu-box-title'>There are many things are needed to start the Fast Food Business.</p>
+              <div className="menu-rating">
+                <button className='menu-add-btn'>
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="menu-boxes">
+              <img src="././public/imgs/meals/burger.png" alt="" />
+              <div className="menu-about">
+                <p>Burger</p>
+                <p>10$</p>
+              </div>
+              <p className='menu-box-title'>There are many things are needed to start the Fast Food Business.</p>
+              <div className="menu-rating">
+                <button className='menu-add-btn'>
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="menu-boxes">
+              <img src="././public/imgs/meals/salad1.png" alt="" />
+              <div className="menu-about">
+                <p>Delmonico Steak dish</p>
+                <p>14$</p>
+              </div>
+              <p className='menu-box-title'>There are many things are needed to start the Fast Food Business.</p>
+              <div className="menu-rating">
+                <button className='menu-add-btn'>
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="menu-boxes">
+              <img src="././public/imgs/meals/salad2.png" alt="" />
+              <div className="menu-about">
+                <p>Egg Masala</p>
+                <p>9$</p>
+              </div>
+              <p className='menu-box-title'>There are many things are needed to start the Fast Food Business.</p>
+              <div className="menu-rating">
+                <button className='menu-add-btn'>
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="menu-boxes">
+              <img src="././public/imgs/meals/salad3.png" alt="" />
+              <div className="menu-about">
+                <p>Peach Melba dish</p>
+                <p>15$</p>
+              </div>
+              <p className='menu-box-title'>There are many things are needed to start the Fast Food Business.</p>
+              <div className="menu-rating">
+                <button className='menu-add-btn'>
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+          <button className="menu-learn-btn">Learn More</button>
+        </div>
+      </div>
+      <div className="app">
+        <img src="././public/imgs/app.png" alt="" className="application" />
+        <div className="app-blog">
+          <div className="app-box">
+            <p className="app-title">Take away</p>
+            <h1 className='app-text'>Food Stalls with Persons but  to  Product marketing plane. </h1>
+            <p className='app-about'>There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also specialized equipmentwith Persons but also  Just Food Stalls with Persons.</p>
+            <div className="app-stores">
+              <button className="appS"> App Store</button>
+              <button className="playM">Play Market</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="last-main-blog">
+        <h1 className="last-main-title">Testimonial</h1>
+        <h1 className="last-main-text">Making Food great again and again</h1>
+        <p className="last-mini-title">You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, Effective Product catlogues etc to make your.</p>
+        <div className="last-main-box">
+          <img src="././public/imgs/last-person.png" alt="" />
+        </div>
+        <div className="last-main-box2">
+          <i class="fa-solid fa-angle-left"></i>
+          <p>You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, Effective Product catlogues etc very successful to make your.</p>
+          <i class="fa-solid fa-chevron-right"></i>
+        </div>
+        <p className="last-main-text2">Augusta W. Reynoso</p>
+        <p className="last-main-about">Front-end developer</p>
+      </div>
+      <div className="contact">
+        <p className="last-main-title">Contact </p>
+        <h1 className="last-main-text">Food Stalls with Persons but also specialized equipment, Skills to manage.</h1>
+        <div className="contact-search">
+          <input placeholder='Enter your message' type="text" />
+          <button>Send</button>
+        </div>
+      </div>
+      <div className="footer">
+        <div>
+          <img src="././public/imgs/logo.png" alt="" />
+          <p>www.ravshanbek's.com</p>
+          <p>ravshanbek@gmail.com</p>
+          <p>phone: +998 97 777 77 07</p>
+        </div>
+        <div>
+          <h1>Home</h1>
+          <p>Landingpage</p>
+          <p>Documentation</p>
+          <p>Referral Program</p>
+          <p>UI & UX Design</p>
+          <p>Web Design</p>
+        </div>
+        <div>
+        <h1>Menu</h1>
+          <p>Landingpage</p>
+          <p>Documentation</p>
+          <p>Referral Program</p>
+          <p>UI & UX Design</p>
+          <p>Web Design</p>
+        </div>
+        <div>
+        <h1>Company</h1>
+          <p>Landingpage</p>
+          <p>Documentation</p>
+          <p>Referral Program</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
